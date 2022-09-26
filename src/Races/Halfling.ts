063df1630eb30d0192905ee2fe20424326d254f3
FabioMartins <fabbio1000@gmail.com>
2022-09-26 18:15:50 -0300

    Feat: Criando a classe "Halfling"

---
+++ src/Races/Halfling.ts
@@ -0,0 +1,19 @@
+import Race from './Race';
+
+export default class Halfling extends Race {
+  protected _maxLifePoints: number;
+  static _racesInstances = 0;
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 60;
+    Halfling._racesInstances += 1;
+  }
+
+  public get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static racesInstance(): number {
+    return Halfling._racesInstances;
+  }
+}
